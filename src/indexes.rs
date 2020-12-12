bdd581f8f7164fa1be3cb1dfa9692ff9f96a1989
Kenta Mukai <r@muk.ai>
2020-12-12 21:23:26 +0900

    :recycle: add mod indexes

---
+++ src/indexes.rs
@@ -0,0 +1,51 @@
+use rocket::request::{FromRequest, Outcome, Request};
+use rocket::response::status;
+
+use crate::config::CONFIG;
+use crate::docker_command::{
+    docker_image_inspect, docker_login, docker_pull_image, docker_run_image,
+};
+use crate::utils::{get_subdomain, is_commit_hash_characters};
+
+pub struct HostHeader(pub String);
+impl<'a, 'r> FromRequest<'a, 'r> for HostHeader {
+    type Error = ();
+
+    fn from_request(request: &'a Request) -> Outcome<Self, Self::Error> {
+        match request.headers().get_one("Host") {
+            Some(host) => match get_subdomain(host.to_string()) {
+                Ok(subdomain) => {
+                    if is_commit_hash_characters(subdomain) {
+                        Outcome::Success(HostHeader(host.to_string()))
+                    } else {
+                        Outcome::Forward(())
+                    }
+                }
+                Err(_) => Outcome::Forward(()),
+            },
+            None => Outcome::Forward(()),
+        }
+    }
+}
+
+#[get("/")]
+pub fn index_with_host_header(host: HostHeader) -> Result<String, status::Custom<String>> {
+    let tag = get_subdomain(host.0.clone())?;
+    let image = format!("{}/{}:{}", CONFIG.registry, CONFIG.repository, tag);
+    if cfg!(debug_assertions) {
+        dbg!(&image);
+    }
+
+    let inspect = docker_image_inspect(&image)?;
+    if !inspect.status.success() {
+        docker_login()?;
+        return docker_pull_image(&image);
+    }
+
+    return docker_run_image(host.0, tag, image);
+}
+
+#[get("/", rank = 2)]
+pub fn index() -> &'static str {
+    "Could you please specify a docker tag."
+}
--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,12 @@
 
 #[macro_use]
 extern crate rocket;
-use rocket::request::{FromRequest, Outcome, Request};
-use rocket::response::status;
 
 mod config;
-use config::CONFIG;
 mod docker_command;
-use docker_command::{docker_image_inspect, docker_login, docker_pull_image, docker_run_image};
+mod indexes;
 mod utils;
-use utils::{get_subdomain, is_commit_hash_characters};
-
-struct HostHeader(pub String);
-impl<'a, 'r> FromRequest<'a, 'r> for HostHeader {
-    type Error = ();
-
-    fn from_request(request: &'a Request) -> Outcome<Self, Self::Error> {
-        match request.headers().get_one("Host") {
-            Some(host) => match get_subdomain(host.to_string()) {
-                Ok(subdomain) => {
-                    if is_commit_hash_characters(subdomain) {
-                        Outcome::Success(HostHeader(host.to_string()))
-                    } else {
-                        Outcome::Forward(())
-                    }
-                }
-                Err(_) => Outcome::Forward(()),
-            },
-            None => Outcome::Forward(()),
-        }
-    }
-}
-
-#[get("/")]
-fn index_with_host_header(host: HostHeader) -> Result<String, status::Custom<String>> {
-    let tag = get_subdomain(host.0.clone())?;
-    let image = format!("{}/{}:{}", CONFIG.registry, CONFIG.repository, tag);
-    if cfg!(debug_assertions) {
-        dbg!(&image);
-    }
-
-    let inspect = docker_image_inspect(&image)?;
-    if !inspect.status.success() {
-        docker_login()?;
-        return docker_pull_image(&image);
-    }
-
-    return docker_run_image(host.0, tag, image);
-}
-
-#[get("/", rank = 2)]
-fn index() -> &'static str {
-    "Could you please specify a docker tag."
-}
+use config::CONFIG;
 
 fn main() {
     if cfg!(debug_assertions) {
@@ -62,6 +16,9 @@ fn main() {
         dbg!(CONFIG.port);
     }
     rocket::ignite()
-        .mount("/", routes![index, index_with_host_header])
+        .mount(
+            "/",
+            routes![indexes::index, indexes::index_with_host_header],
+        )
         .launch();
 }
